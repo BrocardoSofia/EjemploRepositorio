4e7e31365b4a7f5057d19bbced29e674a2614666
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-10-31 18:53:07 -0300

    update

--- main.c
+++ main.c
@@ -4,10 +4,6 @@
 int main()
 {
     printf("Hello world!\n");
-    printf("Soy una linea creada en el branch1");
-    printf("nueva linea");
-    printf("linea nueva");
-    printf("hola");
-    printf("update1");
+
     return 0;
 }
